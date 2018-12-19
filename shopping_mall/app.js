c0085866c54bf40129d5755b0fc8a36fb1f1fa60
etff <wish2rich@gmail.com>
2018-12-19 21:41:51 +0900

    add view engine

--- shopping_mall/app.js
+++ shopping_mall/app.js
@@ -1,11 +1,20 @@
 var express = require("express");
+var path = require("path");
 var admin = require("./routes/admin")
 var app = express();
 var port = 3000;
 
+// 확장자가 ejs 로 끝나는 뷰 엔지을 추가한다.
+app.set("views", path.join(__dirname, "views"));
+console.log(__dirname);
+app.set("view engine", "ejs");
+
+
 app.get('/', function(req, res) {
     res.send("first app");
 });
+
+
 
 app.use("/admin", admin);
 
