77d80ab7a9178f7b68ea79bbad49f0b7437101eb
etff <wish2rich@gmail.com>
2018-12-26 21:43:18 +0900

    add products model

--- shopping_mall/routes/admin.js
+++ shopping_mall/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var ProductsModel = require("../models/ProductsModel");
 
 router.get("/", function(req, res) {
     res.send("admin page");
@@ -15,6 +16,29 @@ router.get("/products", function(req, res) {
 
 router.get("/products/write", function(req, res) {
     res.render("admin/form");
+});
+
+router.post('/products/write', function(req,res){
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    });
+    product.save(function(err){
+        res.redirect('/admin/products');
+    });
+});
+
+router.post("/products/write", function(req, res) {
+    var product = new ProductsModel({
+        name        : req.body.name,
+        price       : req.body.price,
+        description : req.body.description
+    });
+
+    product.save(function(err) {
+        res.redirect("/admin/products");
+    })
 });
 
 module.exports = router;
