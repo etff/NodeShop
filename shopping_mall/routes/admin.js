2a89d5aff32ce0391f819154498bde19275586bf
etff <wish2rich@gmail.com>
2019-01-02 20:22:16 +0900

    add detail method

--- shopping_mall/routes/admin.js
+++ shopping_mall/routes/admin.js
@@ -33,4 +33,12 @@ router.post('/products/write', function(req,res){
     });
 });
 
+router.get("/products/detail/:id", function(req, res) {
+    
+    // url에서 변수 값을 받아올땐 req.params.id 로 받아온다.
+    ProductsModel.findOne( { "id" : req.params.id }, function(err, product) {
+        res.render("admin/productsDetails", { product: product });
+    });
+})
+
 module.exports = router;
