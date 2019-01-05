986e0edf5fb3637221bcceeb86aebc0ed0332eae
etff <wish2rich@gmail.com>
2019-01-05 23:29:13 +0900

    add contact route

--- shopping_mall/routes/contacts.js
+++ shopping_mall/routes/contacts.js
@@ -26,6 +26,26 @@ router.post('/write', function(req,res){
     });
 });
 
+router.get("/edit/:id", function(req, res) {
+
+    ContactsModel.findOne({ id : req.params.id},  function(req, res) {
+        res.render("contacts/form", { contact : contact }); 
+    });
+});
+
+router.post("/edit/:id", function(req, res) {
+
+    var query = {
+        name    : req.body.name,
+        phone   : req.body.phone,
+        email   : req.body.email
+    }
+
+    ContactsModel.update({ id : req.params.id }, { $set : query }, function(err) {
+        res.redirect("/detail/" + req.params.id );  
+    });
+});
+
 router.get("/detail/:id", function(req, res){
     
     //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
